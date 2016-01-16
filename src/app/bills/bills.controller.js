c951d2336ef8b1125b2ad3d5d95f89145c3d3e8e
Cassandra Wilcox <csndra0504@gmail.com>
2016-01-16 12:51:40 -0500

    twitter vote links!!!

--- src/app/bills/bills.controller.js
+++ src/app/bills/bills.controller.js
@@ -14,8 +14,26 @@
         vm.bills = d.data.data;
         vm.meta = d.data.meta;
         vm.links = d.data.links;
+        generateTwitterShareLink(vm.bills)
       })
     };
+
+    function generateTwitterShareLink(bills){
+      angular.forEach(bills, function(bill){
+        var identifier = bill.attributes.identifier;
+        identifier = identifier.replace(/\s/g, '');
+        var billLink = "http://www.flvote.org/"+bill.id
+        billLink = encodeURIComponent(billLink);
+        // console.log(identifier, bill)
+        bill.voteYesLink = 'https://twitter.com/home?status=I%20support%20%23'+identifier+
+        '%20Show%20your%20support,%20vote%20%23'+'yes'+'%20at%20'+billLink+
+        '%20%23flvote%20%23tabsontally%20%40CodeForOrlando%20%40tabsontally';
+        bill.voteNoLink = 'https://twitter.com/home?status=I%20don%E2%80%99t%20support%20%23'+identifier+
+        '%20Stop%20this%20bill%20by%20voting%20vote%20%23'+'no'+'%20at%20'+ billLink+
+        '%20%23flvote%20%23tabsontally%20%40CodeForOrlando%20%40tabsontally%0A;'
+        // console.log(bill.voteYesLink);
+      })
+    }
 
     vm.init = function () {
       console.log('BillsCtrl init');
