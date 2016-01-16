210a3985ae8e6688bc5859a0e274b5470101b12e
Eli Murray <emurray@tampabay.com>
2016-01-16 12:17:40 -0500

    refactored bills svc to match conventions in controllers

--- src/app/services/BillsSvc.js
+++ src/app/services/BillsSvc.js
@@ -1,31 +1,34 @@
 (function() {
-    "use strict";
-    angular.module('flvote')
-        .service('BillsSvc', function($http) {
+  "use strict";
+  angular
+    .module('flvote')
+    .service('BillsSvc', BillsSvc);
 
-            var BASE_CONFIG = {
-                headers: {
-                    //"X-APIKEY": '932407d3-d4bd-4beb-8cd2-f4356036b6fc',
-                    "Content-Type": 'application/vnd.api+json'
-                },
-                method: "GET",
-                url: 'https://www.tabsontallahassee.com/api/bills'
-            };
+  function BillsSvc($http) {
+    var BASE_CONFIG = {
+      headers: {
+        "Content-Type": 'application/vnd.api+json'
+      },
+      method: "GET",
+      url: "https://www.tabsontallahassee.com/api/bills",
+      params: {
+        apikey: '932407d3-d4bd-4beb-8cd2-f4356036b6fc'
+      }
+    };
 
-            this.fetchBillsThisSession = function() {
-                var params = {
-                    params: {
-                        legislative_session: 2015,
-                        apikey: '932407d3-d4bd-4beb-8cd2-f4356036b6fc'
-                    }
-                };
-                var requestConfig = angular.merge({}, params, BASE_CONFIG);
-                return $http(requestConfig);
-            };
+    this.fetchBillsThisSession = function () {
+      var params = {
+        params: {
+          legislative_session: 2015
+        }
+      };
+      var requestConfig = angular.merge({}, params, BASE_CONFIG);
+      return $https(requestConfig);
+    };
 
-            this.fetchBillsWithCustomParams = function(params) {
-                var requestConfig = angular.merge({}, {params: params}, BASE_CONFIG);
-                return $http(requestConfig);
-            };
-        });
+    this.fetchBillsThisSession = function (params) {
+      var requestConfig = angular.merge({}, {params: params}, BASE_CONFIG);
+      return $http(requestConfig)
+    };
+  }
 })();
