b27f15faa5044fbb454b9414ae4c1d1cf14e9591
Cassandra Wilcox <csndra0504@gmail.com>
2016-01-23 13:13:33 -0500

    fix: correct twitter link formatting, bill url & #billID

--- src/app/bill/bill.controller.js
+++ src/app/bill/bill.controller.js
@@ -15,17 +15,18 @@
       BillsSvc.fetchBillByID(billId).then(function(d) {
         vm.bill = d.data.data;
         var identifier = vm.bill.attributes.identifier;
+        var hashIdentifier = identifier.replace(/\s/g, '');
         var billId = vm.bill.id.replace(/\//g, "_");
-        var billLink = "http://www.flvote.org/#/"+billId
+        var billLink = "http://www.flvote.org/"+billId
         billLink = encodeURIComponent(billLink);
         var proms = TwitterSvc.getVotesForSpecificBill(identifier);
         proms.then(function(x){
           vm.bill.twitterVotes = x;
         });
-        vm.bill.voteYesLink = 'https://twitter.com/home?status=I%20support%20%23'+identifier+
+        vm.bill.voteYesLink = 'https://twitter.com/home?status=I%20support%20%23'+hashIdentifier+
         '%20Show%20your%20support,%20vote%20%23'+'yes'+'%20at%20'+billLink+
         '%20%23flvote%20%23tabsontally%20%40CodeForOrlando%20%40tabsontally';
-      vm.bill.voteNoLink = 'https://twitter.com/home?status=I%20don%E2%80%99t%20support%20%23'+identifier+
+        vm.bill.voteNoLink = 'https://twitter.com/home?status=I%20don%E2%80%99t%20support%20%23'+hashIdentifier+
         '%20Stop%20this%20bill%20by%20voting%20vote%20%23'+'no'+'%20at%20'+ billLink+
         '%20%23flvote%20%23tabsontally%20%40CodeForOrlando%20%40tabsontally%0A;'
       })
