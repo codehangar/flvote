cb9d5548a5d7b549a863a2f9539bb5f0ca508b84
Cassandra Wilcox <csndra0504@gmail.com>
2016-01-24 16:26:12 -0500

    chore: add google analytics event tracking to bill page

--- src/app/bills/bills.controller.js
+++ src/app/bills/bills.controller.js
@@ -21,6 +21,13 @@
         vm.links = d.data.links;
         generateTwitterShareLink(vm.bills)
       })
+      // GA tracking
+      ga('send', {
+        'hitType': 'event',
+        'eventCategory': 'User Find Bills',
+        'eventAction': 'Submit Search Query',
+        'eventLabel': query
+        });
     };
 
     function generateTwitterShareLink(bills){
@@ -45,10 +52,31 @@
       })
     }
 
+    vm.voteNo = function(bill){
+      // GA tracking
+      ga('send', {
+        'hitType': 'event',
+        'eventCategory': 'Twitter Button',
+        'eventAction': 'Click Vote #NO',
+        'eventLabel': bill.attributes.identifier + ' ' + bill.attributes.title
+        });
+    }
+
+    vm.voteYes = function(bill){
+      // GA tracking
+      ga('send', {
+        'hitType': 'event',
+        'eventCategory': 'Twitter Button',
+        'eventAction': 'Click Vote #YES',
+        'eventLabel': bill.attributes.identifier + ' ' + bill.attributes.title
+        });
+    }
+
     vm.getNextPage = function() {
       var idx = _.findIndex(vm.allBills, function(bill) {
         return bill.id === vm.bills[vm.bills.length - 1].id
       });
+      console.log('idx',idx)
       if (idx !== vm.allBills.length - 1) {
         vm.bills = vm.allBills.slice(0, idx + 10);
         generateTwitterShareLink(vm.bills.slice(idx, idx + 10))
@@ -60,12 +88,20 @@
             generateTwitterShareLink(vm.bills.slice(idx, idx + 10))
           })
       }
+      ga('send', {
+        'hitType': 'event',
+        'eventCategory': 'User Find Bills',
+        'eventAction': 'Click Load More',
+        'eventLabel': 'Show ' + vm.bills.length + '  results'
+        });
     };
 
     vm.init = function () {
       vm.subject = $stateParams.subject;
 
       vm.fetchBills();
+
+      // vm.searchQuery = $location.search().
     };
 
     vm.init();
