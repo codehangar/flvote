6ecea35edecf3582a6acf96cdd82062d79dac13b
Keith Keough <keithakeough@gmail.com>
2016-02-08 18:40:58 -0500

    fix: move connectionInfo logic into the fs read

--- src/services/RethinkDbService.js
+++ src/services/RethinkDbService.js
@@ -3,27 +3,7 @@ var r = require('rethinkdb'),
 	connectionInfo;
 
 var RethinkDbService = function() {
-	// TODO - We need to move from compose.io to our own DO Box
-	// This is required for now to switch between using ssl or not for local vs prod until then
-	if(process.env.NODE_ENV === 'local') {
-		this.connectionInfo = {
-					host: process.env.DATABASE_HOST,
-					port: process.env.DATABASE_PORT,
-					db: process.env.DATABASE_NAME
-				};
-	}
 
-	if(process.env.NODE_ENV === 'production') {
-		this.connectionInfo = {
-					host: process.env.DATABASE_HOST,
-					port: process.env.DATABASE_PORT,
-					authKey: process.env.DATABASE_KEY,
-					db: process.env.DATABASE_NAME,
-					ssl: {
-						ca: caCert
-					}
-				};
-	}
 };
 
 /**
@@ -34,7 +14,28 @@ RethinkDbService.prototype.getConnection = function() {
 	var _this = this;
 	return new Promise(function(resolve, reject) {
 		fs.readFile('./cacert', function(err, caCert) {
-			r.connect(_this.connectionInfo).then(function(conn) {
+			// TODO - We need to move from compose.io to our own DO Box
+			// This is required for now to switch between using ssl or not for local vs prod until then
+			if(process.env.NODE_ENV === 'local') {
+				connectionInfo = {
+							host: process.env.DATABASE_HOST,
+							port: process.env.DATABASE_PORT,
+							db: process.env.DATABASE_NAME
+						};
+			}
+
+			if(process.env.NODE_ENV === 'production') {
+				connectionInfo = {
+							host: process.env.DATABASE_HOST,
+							port: process.env.DATABASE_PORT,
+							authKey: process.env.DATABASE_KEY,
+							db: process.env.DATABASE_NAME,
+							ssl: {
+								ca: caCert
+							}
+						};
+			}
+			r.connect(connectionInfo).then(function(conn) {
 				resolve(conn);
 			}).catch(function(err) {
 				reject(err);
--- src/tools/databaseSetup.js
+++ src/tools/databaseSetup.js
@@ -13,30 +13,29 @@ var r = require('rethinkdb'),
 		fs = require('fs'),
 		connectionInfo;
 
-// TODO - We need to move from compose.io to our own DO Box
-// This is required for now to switch between using ssl or not for local vs prod until then
-if(process.env.NODE_ENV === 'local') {
-	connectionInfo = {
-				host: process.env.DATABASE_HOST,
-				port: process.env.DATABASE_PORT
-			};
-}
-
-if(process.env.NODE_ENV === 'production') {
-	connectionInfo = {
-				host: process.env.DATABASE_HOST,
-				port: process.env.DATABASE_PORT,
-				authKey: process.env.DATABASE_KEY,
-				ssl: {
-					ca: caCert
-				}
-			};
-}
-
 /* Make database connection */
 function dbConnection() {
 	return new Promise(function(resolve, reject) {
 		fs.readFile('./cacert', function(err, caCert) {
+			// TODO - We need to move from compose.io to our own DO Box
+			// This is required for now to switch between using ssl or not for local vs prod until then
+			if(process.env.NODE_ENV === 'local') {
+				connectionInfo = {
+							host: process.env.DATABASE_HOST,
+							port: process.env.DATABASE_PORT
+						};
+			}
+
+			if(process.env.NODE_ENV === 'production') {
+				connectionInfo = {
+							host: process.env.DATABASE_HOST,
+							port: process.env.DATABASE_PORT,
+							authKey: process.env.DATABASE_KEY,
+							ssl: {
+								ca: caCert
+							}
+						};
+			}
 			r.connect(connectionInfo).then(function(conn) {
 				resolve(conn);
 			}).catch(function(err) {
