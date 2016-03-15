9968a1e4bd25441dc7588cd7ab7328c972c13756
Cassandra Wilcox <csndra0504@gmail.com>
2016-03-15 13:22:47 +0100

    feat: add Disqus comments to bill pages

--- src/public/app/bill/bill.controller.js
+++ src/public/app/bill/bill.controller.js
@@ -5,7 +5,7 @@
     .module('flvote')
     .controller('BillCtrl', BillCtrl);
 
-  function BillCtrl(TwitterSvc, BillsSvc, $stateParams) {
+  function BillCtrl(TwitterSvc, BillsSvc, $stateParams, $window) {
 
     var vm = this;
 
@@ -19,11 +19,35 @@
 
     };
 
+    vm.initDisqus = function(){
+      var disqus_config = function () {
+          this.page.url = vm.canonicalUrl;  // Replace PAGE_URL with your page's canonical URL variable
+          this.page.identifier = $stateParams.id; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      
+      (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
+          var d = document, s = d.createElement('script');
+          
+          s.src = '//FLVOTE.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
+          
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s);
+      })();
+    }
+
     vm.init = function () {
       vm.fetchBill();
       vm.hashtagBill = "HB409";
       vm.hashtagYes = "yes";
       vm.billLink = encodeURIComponent("http://www.google.com");
+      vm.canonicalUrl = $window.location.href;
+      
+      vm.disqusConfig = {
+          disqus_shortname: 'FLVOTE',
+          disqus_identifier: $stateParams.id,
+          disqus_url: $window.location.href
+      };
+
     };
 
     vm.init();
