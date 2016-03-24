3204b22d1dd3045a189cb1c3453821e74787ae65
Cassandra Wilcox <csndra0504@gmail.com>
2016-03-24 15:40:28 +0100

    style: fixed header

--- src/public/app/services/TwitterVotesSvc.js
+++ src/public/app/services/TwitterVotesSvc.js
@@ -9,25 +9,9 @@
         },
         method: "GET",
         url: "/api/v1/votes",
-        params: {
-          // apikey: '932407d3-d4bd-4beb-8cd2-f4356036b6fc',
-          // legislative_session: 2016,
-        }
+        params: {}
       };
 
-      // this.fetchBillsThisSession = function (params) {
-      //   var config = {
-      //     params: params
-      //   };
-      //   var requestConfig = angular.merge({}, config, BASE_CONFIG);
-      //   console.log("requestConfig", requestConfig)
-      //   return $http(requestConfig);
-      // };
-
-      // this.fetchBillsCustomParams = function (params) {
-      //   var requestConfig = angular.merge({}, {params: params}, BASE_CONFIG);
-      //   return $http(requestConfig)
-      // };
       this.fetchVotesForBill = function(bill) {
         console.log('bill',bill);
         BASE_CONFIG.params.url = BASE_CONFIG.params.url + '/' + bill.attributes.identifier
@@ -36,9 +20,7 @@
 
       this.fetchVotesForBills = function(bills) {
         var billIds = [];
-        console.log('bill',bills);
         angular.forEach(bills, function(bill){
-
           billIds.push(bill.attributes.identifier);
         });
         billIds = billIds.join(',')
@@ -46,17 +28,5 @@
         return $http(BASE_CONFIG);
       };
 
-      // this.fetchVoteByID = function(bills) {
-      //   // voteId = voteId.replace('_','/');
-      //   BASE_CONFIG.params.url = 'https://www.tabsontallahassee.com/api/'+voteId;
-      //   // var requestConfig = angular.merge({}, BASE_CONFIG, {bill: voteId});
-      //   // console.log("requestConfig", BASE_CONFIG)
-      //   return $http(BASE_CONFIG);
-      // };
-
-      // this.fetchNext = function(url) {
-      //   var requestConfig = angular.merge({}, BASE_CONFIG, {url: url});
-      //   return $http(requestConfig);
-      // }
     });
 })();
