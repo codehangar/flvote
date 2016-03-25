644bf4192cdf2c59d7609673bcbc7ef2889e188f
Ian Grail <grailians@gmail.com>
2016-03-25 13:10:24 +0100

    fix: disqus comments on bill detail page

--- src/public/app/bill/bill.controller.js
+++ src/public/app/bill/bill.controller.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   angular
@@ -9,39 +9,35 @@
 
     var vm = this;
 
-    vm.fetchBill = function () {
+    vm.fetchBill = function() {
       var billId = decodeURIComponent($stateParams.id);
 
       BillsSvc.fetchBillByID(billId).then(function(d) {
         vm.bill = d.data.data;
-        console.log('vm.bill',vm.bill)
+        console.log('vm.bill', vm.bill)
         getTwitterVotesForBills(vm.bill);
         TwitterSvc.addTwitterLinksToBill(vm.bill);
-        vm.disqusConfig.disqus_title = vm.bill.attributes.identifier +': '+vm.bill.attributes.title;
+
+        vm.disqusConfig = {
+          disqus_shortname: 'flvote',
+          disqus_identifier: $window.location.href,
+          disqus_url: $window.location.href,
+          disqus_title: vm.bill.attributes.identifier + ': ' + vm.bill.attributes.title
+        };
       })
     };
 
-    function getTwitterVotesForBills(bill){
+    function getTwitterVotesForBills(bill) {
       TwitterVotesSvc.fetchVotesForBill(bill).then(function(d) {
         var twitterVote = d.data[0];
-        if(bill.attributes.identifier === twitterVote.billIdentifier)
+        if (bill.attributes.identifier === twitterVote.billIdentifier)
           bill.twitterVotes = twitterVote;
       })
     }
 
-    vm.init = function () {
+    vm.init = function() {
       vm.fetchBill();
-      vm.hashtagBill = "HB409";
-      vm.hashtagYes = "yes";
-      vm.billLink = encodeURIComponent("http://www.google.com");
-      vm.canonicalUrl = $window.location.href;
-
-      vm.disqusConfig = {
-          disqus_shortname: 'flvote',
-          disqus_identifier: $window.location.href,
-          disqus_url: $window.location.href
-      };
-
+      vm.disqusConfig = {};
     };
 
     vm.init();
